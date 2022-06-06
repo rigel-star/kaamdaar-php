9d1e4fef898d0bf5596f65c8f5ee09fbf7435d40
Ramesh Poudel <rigelstar@Rameshs-MacBook-Air.local>
2022-06-06 13:45:54 +0545

    default fonts and css added for settings page

--- views/settings.php
+++ views/settings.php
@@ -2,26 +2,44 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../static/css/nav.css">
         <link rel="stylesheet" href="../static/css/settings.css">
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+
         <title>Settings</title>
+
+        <style>
+            .nav-link-active
+			{
+				color: blue;
+				background-color: #E1EDFF;
+				display: block;
+				height: 40px;
+				border-radius: 0 8px 8px 0;
+				margin-left: 0px;
+				transform: translateX(-20px);
+				padding: 10px 0 0 20px;
+			}
+        </style>
     </head>
     <body>
         <div class="root">
             <div class="page-head">
-                    <div>
-                        <h2>Kaamdaar</h2>
-                    </div>
-                    <div>
-                        <input type="text" class="search-bar" placeholder="Search">
-                    </div>
+                <div>
+                    <h2>Kaamdaar</h2>
+                </div>
+                <div>
+                    <input type="text" class="search-bar" placeholder="Search">
+                </div>
 			</div>
             <div class="page-body">
                 <div id="nav-bar" class="nav-bar">
-					<a href="profile.php" class="nav-link"><span class="nav-link-active"><i class="fa fa-user" style="font-size:24px;"></i> Profile</span></a>
+					<a href="profile.php" class="nav-link"><i class="fa fa-user" style="font-size:24px;"></i> Profile</a>
 					<a href="bprofile.php" class="nav-link"><i class="fa fa-briefcase" style="font-size:24px"></i> Business Profile</a>
 					<a href="requests.php" class="nav-link"><i class="fa fa-send" style="font-size:24px"></i> Your requests</a>
 					<a href="notifications.php" class="nav-link"><i class="fa fa-bell" style="font-size:24px"></i> Notifications</a>
@@ -30,30 +48,30 @@
 					<a href="add-business.php" class="nav-link">Add new business</a>
 					<hr>
 					<a href="logout.php" class="nav-link"><i class="fa fa-sign-out" style="font-size:24px"></i>Logout</a>
-					<a href="#" class="nav-link"><i class="fa fa-cog" style="font-size:24px"></i> Settings</a>
+					<a href="#" class="nav-link"><span class="nav-link-active"><i class="fa fa-cog" style="font-size:24px"></i> Settings</span></a>
 					<a href="#" class="nav-link"><i class="fa fa-print" style="font-size:24px"></i> Privacy policy</a>
 				</div>
-            </div>
-            <div class="page-content">
-                <div class="pc-title">
-                    <div class="pct-1">
-                        <div class="pc-main-title">
-                            <h2>Settings</h2>
-                        </div>
-                        <div class="pc-sub-title">
-                            <h4>Change settings</h4>
+                <div class="page-content">
+                    <div class="pc-title">
+                        <div class="pct-1">
+                            <div class="pc-main-title">
+                                <h2>Settings</h2>
+                            </div>
+                            <div class="pc-sub-title">
+                                <h4>Change settings</h4>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="setting-list">
-                    <div class="setting">
-                        <div class="setting-text">
-                            <p>
-                                Setting text
-                            </p>
-                        </div>
-                        <div class="setting-action">
-                            <button>Setting action</button>
+                    <div class="setting-list">
+                        <div class="setting">
+                            <div class="setting-text">
+                                <p>
+                                    Setting text
+                                </p>
+                            </div>
+                            <div class="setting-action">
+                                <button>Setting action</button>
+                            </div>
                         </div>
                     </div>
                 </div>
