f3db3f4dc6941183f53a6f870b0e1ea8468cc90e
Ramesh Poudel <rigelstar@Rameshs-MacBook-Air.local>
2022-06-06 10:37:42 +0545

    settings page boilerplate

---
+++ views/settings.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../static/css/nav.css">
+        <link rel="stylesheet" href="../static/css/settings.css">
+
+        <title>Settings</title>
+    </head>
+    <body>
+        <div class="root">
+            <div class="page-head">
+                    <div>
+                        <h2>Kaamdaar</h2>
+                    </div>
+                    <div>
+                        <input type="text" class="search-bar" placeholder="Search">
+                    </div>
+			</div>
+            <div class="page-body">
+                <div id="nav-bar" class="nav-bar">
+					<a href="#" class="nav-link"><span class="nav-link-active"><i class="fa fa-user" style="font-size:24px;"></i> Profile</span></a>
+					<a href="bprofile.php" class="nav-link"><i class="fa fa-briefcase" style="font-size:24px"></i> Business Profile</a>
+					<a href="requests.php" class="nav-link"><i class="fa fa-send" style="font-size:24px"></i> Your requests</a>
+					<a href="notifications.php" class="nav-link"><i class="fa fa-bell" style="font-size:24px"></i> Notifications</a>
+					<hr>
+					<a href="add-request.php" class="nav-link">Add new request</a>
+					<a href="add-business.php" class="nav-link">Add new business</a>
+					<hr>
+					<a href="logout.php" class="nav-link"><i class="fa fa-sign-out" style="font-size:24px"></i>Logout</a>
+					<a href="#" class="nav-link"><i class="fa fa-cog" style="font-size:24px"></i> Settings</a>
+					<a href="#" class="nav-link"><i class="fa fa-print" style="font-size:24px"></i> Privacy policy</a>
+				</div>
+            </div>
+            <div class="page-content">
+                <div class="pc-title">
+                    <div class="pct-1">
+                        <div class="pc-main-title">
+                            <h2>Settings</h2>
+                        </div>
+                        <div class="pc-sub-title">
+                            <h4>Change settings</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="setting-list">
+                    <div class="setting">
+                        <div class="setting-text">
+                            <p>
+                                Setting text
+                            </p>
+                        </div>
+                        <div class="setting-action">
+                            <button>Setting action</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
