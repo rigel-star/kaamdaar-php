31c376755fd1fc294eba6b88e0fdb4b158da6437
Ramesh Poudel <rigelstar@Rameshs-MacBook-Air.local>
2022-06-09 23:44:17 +0545

    request related constants

---
+++ views/request-constants.php
@@ -0,0 +1,15 @@
+<?php
+const REQUEST_TYPE_NAMES = [
+    10 => "Auto Mobile Repair",
+    20 => "Plumbing",
+    30 => "Carpenting",
+    40 => "Computer Repair",
+    50 => "House Cleaning",
+    60 => "Painter",
+];
+
+const REQUEST_STATUS = [
+    0 => "Pending",
+    1 => "Fulfilled"
+];
+?>
