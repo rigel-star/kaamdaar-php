38b2d4b4b1c0862753d64acb5bc33f7c51becbea
Ramesh Poudel <rigelstar@Rameshs-MacBook-Air.local>
2022-06-14 02:15:21 +0545

    base class for notification types
    Notification id, title, description, creation date and icon are
    common variable in request and response notifications.

---
+++ models/notif/notification.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Notification 
+{
+    public int      $id;
+    public string   $title;
+    public string   $description;
+    public string   $creationDate;
+    public string   $icon;
+
+    public function __construct(int $id, string $title, string $description, string $creationDate, string $icon)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->creationDate = $creationDate;
+        $this->icon = $icon;
+    }
+}
+?>
