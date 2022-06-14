4daf95100229bec70d5b54cbfa1159817dc5d3bc
Ramesh Poudel <rigelstar@Rameshs-MacBook-Air.local>
2022-06-14 21:26:09 +0545

    request and reponse notification models

--- models/notif/notification.php
+++ models/notif/notification.php
@@ -1,4 +1,10 @@
 <?php 
+namespace Model;
+
+require_once "../response.php";
+require_once "../request.php";
+
+use Model\{Request, Response};
 
 abstract class Notification 
 {
@@ -15,6 +21,28 @@ abstract class Notification
         $this->description = $description;
         $this->creationDate = $creationDate;
         $this->icon = $icon;
+    }
+}
+
+class RequestNotification extends Notification
+{
+    public Request $request;
+
+    public function __construct(int $id, Request $request, string $description)
+    {
+        parent::__construct($id, REQUEST_TYPE_NAMES[$request->type], $description, $request->time, "");
+        $this->request = $request;
+    }
+}
+
+class ResponseNotification extends Notification
+{
+    public Response $response;
+
+    public function __construct(int $id, Response $response, string $title)
+    {
+        parent::__construct($id, $title, "", $response->responseDate, "");
+        $this->response = $response;
     }
 }
 ?>
