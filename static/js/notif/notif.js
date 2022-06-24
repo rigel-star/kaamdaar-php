9a218979e03a2660f3da1850d13588270e6950e9
Ramesh Poudel <rigelstar@Rameshs-MacBook-Air.local>
2022-06-24 09:52:00 +0545

    function to create HTML strucutre for Notification class
    createHTML() function returns the html representation of
    a given notification model.

--- static/js/notif/notif.js
+++ static/js/notif/notif.js
@@ -8,6 +8,57 @@ class Notification
         this.description = description;
         this.date = date;
     }
+
+    createHTML()
+    {
+        let notifRoot = document.createElement("div");
+        notifRoot.classList.add("notif-root", "notif-unread");
+
+        let notifHead = document.createElement("div");
+        notifHead.classList.add('notif-head');
+
+        let notifHeadIcon = document.createElement("div");
+        notifHeadIcon.classList.add('notif-head-icon');
+
+        let notifHeadIconImg = document.createElement("img");
+        notifHeadIconImg.classList.add('notif-head-icon-img');
+        notifHeadIconImg.src = this.icon;
+
+        let notifHeadTitle = document.createElement("div");
+        notifHeadTitle.classList.add('notif-head-title');
+
+        let notifProfile = document.createElement("p");
+        notifProfile.classList.add('notif-profile');
+        notifProfile.innerText = this.profile;
+
+        let notifTitle = document.createElement("p");
+        notifTitle.classList.add('notif-title');
+        notifTitle.innerText = this.title;
+
+        let notifBody = document.createElement("div");
+        notifBody.classList.add('notif-body');
+
+        let notifDesc = document.createElement("p");
+        notifDesc.classList.add('notif-desc');
+        notifDesc.innerText = this.description;
+
+        let notifDate = document.createElement("p");
+        notifDate.classList.add('notif-date');
+        notifDate.innerText = this.date;
+
+        notifHeadIcon.appendChild(notifHeadIconImg);
+        notifHeadTitle.appendChild(notifProfile);
+        notifHeadTitle.appendChild(notifTitle);
+        notifHead.appendChild(notifHeadIcon);
+        notifHead.appendChild(notifHeadTitle);
+
+        notifBody.appendChild(notifDesc);
+        notifBody.appendChild(notifDate);
+
+        notifRoot.appendChild(notifHead);
+        notifRoot.appendChild(notifBody);
+        return notifRoot;
+    }
 }
 
 class RequestNotification extends Notification
