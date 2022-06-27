eed512b12076f86c459a7a9044549d17adad71a1
Ramesh Poudel <rigelstar@Rameshs-MacBook-Air.local>
2022-06-27 20:31:31 +0545

    function to calculate relative time from date

--- static/js/notif/notif-modal.js
+++ static/js/notif/notif-modal.js
@@ -55,7 +55,7 @@ function showNotificationModal()
                         profile: notification.PROFILE,
                         title: notification.TITLE,
                         description: notification.DESCRIPTION,
-                        time: notification.TIME,
+                        time: timeSince(new Date(notification.TIME)) + " ago",
                         status: notification.STATUS,
                         requestId: notification.REQUEST_ID
                     }
@@ -121,4 +121,30 @@ function showNotificationModal()
             }
         }
     };
+}
+
+function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = seconds / 31536000;
+
+    if (interval > 1) 
+        return Math.floor(interval) + " years";
+
+    interval = seconds / 2592000;
+    if (interval > 1)
+        return Math.floor(interval) + " months";
+
+    interval = seconds / 86400;
+    if (interval > 1)
+        return Math.floor(interval) + " days";
+
+    interval = seconds / 3600;
+    if (interval > 1)
+        return Math.floor(interval) + " hours";
+
+    interval = seconds / 60;
+    if (interval > 1)
+        return Math.floor(interval) + " minutes";
+
+    return Math.floor(seconds) + " seconds";
 }
