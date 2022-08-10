fa4f0bda31a787f5f17fbbc0478fce534ab84c57
Ramesh Poudel <rigelstar@Rameshs-MacBook-Air.local>
2022-08-10 14:33:39 +0545

    added description as get request field in add new business module

--- views/add-newb.php
+++ views/add-newb.php
@@ -5,14 +5,15 @@ require_once "../constants.php";
 require_once ROOT_DIR . "controllers/db/kaamdaar_orm.php";
 
 $type = $_GET['type'];
+$desc = $_GET['desc'];
 $bid = $_SESSION['business_id'];
 
-$SQL = "INSERT INTO business(BUSINESS_TYPE, BUSINESS_START_DATE, B_PROFILE_ID) VALUES('$type', DEFAULT, '" . $bid . "');";
+$SQL = "INSERT INTO business(BUSINESS_TYPE, BUSINESS_START_DATE, B_PROFILE_ID, BUSINESS_STATUS, BUSINESS_DESC) VALUES($type, DEFAULT, '$bid', 0, '$desc');";
 
 $orm = new KaamdaarORM();
 
 if(!$orm->connection->query($SQL))
-    header('HTTP/1.1 500 Internal Server Error');
+    header("HTTP/1.1 500 $SQL");
 
 $orm->close();
 ?>
